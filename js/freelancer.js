cd96ae46686ab4b8f0425797a0b1ad120e30e73a
sebastian-minatti <sebmin@gmail.com>
2016-06-02 21:44:45 -0300

    adding slides and new styles

--- js/freelancer.js
+++ js/freelancer.js
@@ -35,3 +35,14 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
